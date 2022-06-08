b140b9809a50add5143ad8049293970bb62c77bb
LoicFort <loicfort@free.fr>
2022-06-08 10:07:23 +0200

    un peu d'effets

--- public/js/laRoueLibre.js
+++ public/js/laRoueLibre.js
@@ -20,13 +20,13 @@ const wrapper = document.querySelector('.wrapper');
 
 
 
-wrapper.addEventListener('scroll', () => { 
-  
+wrapper.addEventListener('scroll', () => {
+
   if (wrapper.scrollTop < 200) {
     backHome.style.display = "none";
     backHomeMobile.style.display = "none";
-;
-  } else if (wrapper.clientWidth > 850){
+    ;
+  } else if (wrapper.clientWidth > 850) {
     backHome.style.display = "block";
   } else {
     backHomeMobile.style.display = "block"
@@ -38,6 +38,7 @@ wrapper.addEventListener('scroll', () => {
 // title animation ---------------------------------------------------------------------------------
 const title = document.querySelectorAll('.presentation h3');
 
+
 window.addEventListener('load', () => {
   const titleAnimation = gsap.timeline({ paused: true });
   titleAnimation.staggerFrom(title, 2, { x: 1000, opacity: 0, ease: "in-and-out" }, 1);
@@ -46,29 +47,58 @@ window.addEventListener('load', () => {
 
 // H5 animation -------------------------------------------------------------
 
-const titles = document.querySelectorAll('h5')
+const titles = document.querySelectorAll('h5');
+const cards = document.querySelectorAll('.card');
+const eventsCard = document.querySelectorAll('#eventsPage .event__card');
 
-
+// wrapper already declare above
 wrapper.addEventListener('scroll', () => {
+
   const { scrollTop, clientHeight } = document.documentElement;
   let h5FromTop = [];
+  let cardsFromTop = [];
+  let eventsCardsFromTop = [];
 
   for (let i = 0; i < titles.length; i++) {
-    h5FromTop.push(titles[i].getBoundingClientRect().top)
-
+    h5FromTop.push(titles[i].getBoundingClientRect().top);
   }
+
+  for (let i = 0; i < cards.length; i++) {
+    cardsFromTop.push(cards[i].getBoundingClientRect().top);
+  }
+
+  eventsCard.forEach((card) => eventsCardsFromTop.push(card.getBoundingClientRect().top))
+
 
   for (let i = 0; i < titles.length; i++) {
     if (scrollTop > (scrollTop + h5FromTop[i]).toFixed() - clientHeight * 0.80) {
-            titles[i].classList.add('active')
+      titles[i].classList.add('active')
     } else {
       titles[i].classList.remove('active')
-
     }
 
   }
+  for (let i = 0; i < cards.length; i++) {
+    if (scrollTop > (scrollTop + cardsFromTop[i]).toFixed() - clientHeight * 0.80) {
+      cards[i].classList.add('active')
+    } else {
+      cards[i].classList.remove('active')
+    }
 
-})
+  }
+  for (let i = 0; i < eventsCard.length; i++) {
+    if (scrollTop > (scrollTop + eventsCardsFromTop[i]).toFixed() - clientHeight * 0.80) {
+      eventsCard[i].classList.add('active')
+    } else {
+      eventsCard[i].classList.remove('active')
+    }
+
+  } 
+
+
+  })
+
+
 
 // Deploy contact container (.gafa) to show more info--------------------------------------------------------
 
@@ -83,13 +113,13 @@ deployMoreInfo.addEventListener('click', () => {
   socialMediaContainer.style.width = socialMediaContainer.style.width === '' ? '350px' : '';
   socialMediaContainer.style.transition = "0.8s ease";
   document.querySelectorAll(".socialMedia > p").forEach((p) => {
-    p.classList.toggle('--hidden');    
-    p.style.width = "85%";   
+    p.classList.toggle('--hidden');
+    p.style.width = "85%";
     p.style.textAlign = "center";
   })
   document.querySelectorAll(".socialMedia").forEach((div) => {
-    div.style.display="flex";
-  } )
+    div.style.display = "flex";
+  })
   // const containerAnimation = gsap.timeline({ paused: true });
   // containerAnimation.staggerFrom(socialMediaContainer, 1, { width: "64px", ease: "in-and-out" });
   // containerAnimation.play();
@@ -143,8 +173,8 @@ document.querySelectorAll('.number').forEach((number) => {
   number.addEventListener('click', () => {
     let phoneNumber = number.textContent;
     // phoneNumber.setSelectionRange(0,20);
-    navigator.clipboard.writeText(phoneNumber);  
-  alert("cliquer sur OK pour copier le numéro " + phoneNumber);    
+    navigator.clipboard.writeText(phoneNumber);
+    alert("cliquer sur OK pour copier le numéro " + phoneNumber);
   })
 
 });
